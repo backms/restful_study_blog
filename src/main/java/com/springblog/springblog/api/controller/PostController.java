ef629950da79fb3457b2584890beec39ef3a9146
backms <iphonex@kakao.com>
2024-09-11 20:37:43 +0900

    - Post

--- src/main/java/com/springblog/springblog/api/controller/PostController.java
+++ src/main/java/com/springblog/springblog/api/controller/PostController.java
@@ -1,8 +1,6 @@
 package com.springblog.springblog.api.controller;
 
 import com.springblog.springblog.api.requset.PostCreate;
-import com.springblog.springblog.api.service.PostService;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
@@ -17,15 +15,23 @@ import java.util.Map;
 
 @Slf4j
 @RestController
-@RequiredArgsConstructor    // service 호출 시 사용되는..
 public class PostController {
 
-    private final PostService postService;
-
     @PostMapping("/posts")
-    public Map<String, String> post(@RequestBody @Valid PostCreate request) {    //@Valid 어노테이션 -> PostCreate의  @NotBlank 연계
+    public Map<String, String> post(@RequestBody @Valid PostCreate params, BindingResult result) {    //@Valid 어노테이션 -> PostCreate의  @NotBlank 연계
+        log.info("params={}", params.toString());
+        if(result.hasErrors()){
+            List<FieldError> filedErrors = result.getFieldErrors();
+            FieldError firstFieldError =  filedErrors.get(0);
 
-        postService.write(request);
+            String filedName = firstFieldError.getField();
+            String errorMessage = firstFieldError.getDefaultMessage();
+
+            Map<String, String> error = new HashMap<>();
+            error.put(filedName, errorMessage);
+
+            return error;
+        }
 
         return Map.of();
     }
--- src/main/java/com/springblog/springblog/api/requset/PostRespository.java
+++
@@ -1,7 +0,0 @@
-package com.springblog.springblog.api.requset;
-
-import com.springblog.springblog.api.domain.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRespository extends JpaRepository<Post, Long> {
-}
