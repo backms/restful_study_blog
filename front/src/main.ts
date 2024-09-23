15bdaaf739f93f3c3fc324188524ea290d69e0c1
backms <iphonex@kakao.com>
2024-09-23 23:03:23 +0900

    - template 설치

--- front/src/main.ts
+++ front/src/main.ts
@@ -6,6 +6,8 @@ import { createPinia } from 'pinia'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
+import "bootstrap/dist/css/bootstrap-utilities.css"
+
 import App from './App.vue'
 import router from './router'
 
