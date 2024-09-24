61b033b302ac73179f85e3f302e2b92955f3ac77
backms <iphonex@kakao.com>
2024-09-24 22:53:57 +0900

    - cors 처리

---
+++ src/main/java/com/springblog/springblog/api/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.springblog.springblog.api.config;
+
+import com.querydsl.core.annotations.Config;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("http://localhost:5173")
+//                .allowedMethods("POST", "GET", "PATCH", "DELETE");
+//    }
+}
