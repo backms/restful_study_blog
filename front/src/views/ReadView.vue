9d9e106f93fa0c8f0afbf24ce5344b365775851f
backms <iphonex@kakao.com>
2024-09-26 19:50:29 +0900

    - 글 내용 화면

---
+++ front/src/views/ReadView.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+
+import {onMounted} from "vue";
+import {ref} from "vue";
+import axios from "axios";
+
+const props = defineProps({
+  postId: {
+    type: [String, Number],
+    required: true,
+  }
+});
+
+const post = ref({
+  id: 0,
+  title: "",
+  content: "",
+});
+
+onMounted(() => {
+  axios.get(`/api/posts/${props.postId}`).then((res) => {
+    post.value = res.data;
+  });
+})
+
+</script>
+
+<template>
+  <h2>{{post.title}}</h2>
+  <div>{{post.content}}</div>
+</template>
+
+<script>
+
+</script>
