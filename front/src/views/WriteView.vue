51fb80aa645c342c1b513aca488a3865a726b66a
backms <iphonex@kakao.com>
2024-09-28 22:01:02 +0900

    - 글 화면 수정

--- front/src/views/WriteView.vue
+++ front/src/views/WriteView.vue
@@ -23,15 +23,19 @@ const write = () => {
 
 <template>
   <div>
-    <el-input v-model="title" type="text" placeholder="제목을 입력해주세요."/>
-  </div>
+    <div>
+      <el-input v-model="title" placeholder="제목을 입력해주세요" />
+    </div>
 
-  <div class="mt-2">
-    <el-input v-model="content" type="textarea" rows="15"></el-input>
-  </div>
+    <div class="mt-2">
+      <el-input v-model="content" type="textarea" rows="15" />
+    </div>
 
-  <div class="mt-2">
-    <el-button type="primary" @click="write()">글 작성완료</el-button>
+    <div class="mt-2">
+      <div class="d-flex justify-content-end">
+        <el-button type="primary" @click="write()">작성완료</el-button>
+      </div>
+    </div>
   </div>
 </template>
 
